6b5848f0dab616619c40a0be0ecbdd9768612be4
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-01-17 12:40:35 -0500

    Reverse String Assignment Finished

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,15 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
+
 function reverseStr(someStr) {
-  
+    let newString="";
+    let srtLength = someStr.length;
+    for (let i = srtLength - 1; i >= 0; i--) {
+        newString += someStr[i];
+        
+    }
+    return newString;
 }
+
+console.log(reverseStr("Dunieski"));
