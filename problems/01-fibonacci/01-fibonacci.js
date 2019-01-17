fb93c28a16a3d4c66f5eb65b03af070dd1b28049
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-01-17 11:16:40 -0500

    Fibonacci Assignment Finished

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,18 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+    if (n == 0 || n == 1) {
+        return n;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
 }
+
+
+for (let i = 0; i < 15; i++) {
+    console.log(fib(i));
+}
+
+
+
+
