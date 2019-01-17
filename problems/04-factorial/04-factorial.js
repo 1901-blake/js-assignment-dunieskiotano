a487f8ff1e81a143d6929dd12c335a0417c266ac
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-01-17 12:53:23 -0500

    Factorial Assignment Finished

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -1,6 +1,16 @@
 /* 4. Factorial
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
-function factorial(sumNum) {
-  
+function factorial(someNum) {
+    let productFactorial = someNum;
+    if (someNum == 1 ) {
+     return 1;
+    }
+    return productFactorial*= factorial(someNum - 1);
+   // return productFactorial;
+}
+
+//pass numbers from 1 to 5 to the function factorial
+for (let i = 1; i <=5; i++) {
+    console.log(factorial(i));
 }
