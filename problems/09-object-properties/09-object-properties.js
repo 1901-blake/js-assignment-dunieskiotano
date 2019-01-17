0841080d87f84797e19dd54c99a30640dd2d207f
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-01-17 13:16:42 -0500

    Object-Properties Assignment Finished

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,43 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
+    //I am using destructuring here for my convenience
+    let { firstName, lastName, age, degree, homecity, countryOfCitizenship, countryOfOrigin } = someObj;
+    //This is one way to access the properties of the object person
+    console.log();
+    console.log("========= One Way using Destructuring =============");
+    console.log(`
+    First Name: ${firstName}
+    Last Name: ${lastName}
+    Age: ${age}
+    Degree: ${degree}
+    Home City: ${homecity}
+    Country of Citizenship: ${countryOfCitizenship}
+    Country of Origin: ${countryOfOrigin}`)
 
+    console.log();
+    console.log("========= Another way using Dot Notation =============");
+    console.log();
+    console.log(
+        '\xa0\xa0\xa0\ First Name: ', someObj.firstName, '\n' +
+        '\xa0\xa0\xa0\ Last Name: ', someObj.lastName, '\n' +
+        '\xa0\xa0\xa0\ Age: ', someObj.age, '\n' +
+        '\xa0\xa0\xa0\ Degree: ', someObj.degree, '\n' +
+        '\xa0\xa0\xa0\ Home City: ', someObj.homecity, '\n' +
+        '\xa0\xa0\xa0\ Country of Citizenship: ', someObj.countryOfCitizenship, '\n' +
+        '\xa0\xa0\xa0\ Country of Origin: ', someObj.countryOfOrigin, '\n' +
+        '\xa0\xa0\xa0\ Function details: ', someObj.whatIdo
+    )
 }
+
+let person = {
+    firstName: 'Dunieski',
+    lastName: 'Otano',
+    age: 42,
+    degree: 'Software Engineering',
+    homecity: 'Miami, FL',
+    countryOfCitizenship: 'U.S.A',
+    countryOfOrigin: 'Cuba',
+    whatIdo: function () { console.log("I study programming") }
+}
+objectProperties(person);
