ad52119463aa7e4d1bd0bed788233218579ee1b0
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-01-17 13:34:03 -0500

    Shapes Assignment Completed

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,60 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
+  let str;
+  newChar = '';
+  let space = ' ';
+  switch (shape) {
+    case 'Square':
+      console.log("===== Square =====");
+      console.log();
+      for (let i = 0; i < height; i++) {
+        let arr = [];
+        for (let j = 0; j < height; j++) {
+          arr.push(character);
+        }
+        console.log(arr.join(''));
+      }
+      console.log();
+      break;
+
+    case 'Triangle':
+      console.log("===== Triangle =====");
+      console.log();
+      for (let i = 0; i < height; i++) {
+        console.log(newChar += character);
+      }
+      console.log();
+      break;
+
+    case 'Diamond':
+      console.log("===== Diamond =====");
+      console.log();
+      let halfHeight = Math.ceil(height / 2);
+      for (let i = 0; i < height; i++) {
+
+        str = ' ';
+        for (let j = 1; j < height - i; j++) {
+          str = str + ' ';
+        }
+        for (let k = 1; k <= (2 * i + 1); k++) {
+          str = str + '*';
+
+        }
+        if (i < halfHeight)
+          console.log(str)
+        else
+          break;
+      }
+
+
+      for (let i = 1; i <= halfHeight - 1; i++) {
+        console.log(space.repeat(i + (halfHeight - 1)), character.repeat(2 * (halfHeight - i) - 1));
+      }
+      console.log();
+      break;
+  }
 }
+printShape("Square", 3, "%");
+printShape("Triangle", 3, "$");
+printShape("Diamond", 5, "*");
